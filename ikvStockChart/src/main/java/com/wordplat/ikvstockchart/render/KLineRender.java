47b699a929ec132ce3847d5ff0328bb05a0a70c5
Andrey Khitryy <andrey.khitryy@gmail.com>
2021-04-19 11:17:19 +0300

    Remove default highlight drawing

--- ikvStockChart/src/main/java/com/wordplat/ikvstockchart/render/KLineRender.java
+++ ikvStockChart/src/main/java/com/wordplat/ikvstockchart/render/KLineRender.java
@@ -109,7 +109,6 @@ public class KLineRender extends AbstractRender {
     private final CandleDrawing candleDrawing = new CandleDrawing();
     private final MADrawing maDrawing = new MADrawing();
     private final EmptyDataDrawing emptyDataDrawing = new EmptyDataDrawing();
-    private final HighlightDrawing highlightDrawing = new HighlightDrawing();
 
     private final List<StockIndex> stockIndexList = new ArrayList<>(); // 股票指标列表
 
@@ -120,7 +119,6 @@ public class KLineRender extends AbstractRender {
         kLineDrawingList.add(candleDrawing);
         kLineDrawingList.add(maDrawing);
         kLineDrawingList.add(emptyDataDrawing);
-        kLineDrawingList.add(highlightDrawing);
 
         zoomAnimator.setDuration(ZOOM_DURATION);
         zoomAnimator.setInterpolator(new LinearInterpolator());
@@ -164,10 +162,6 @@ public class KLineRender extends AbstractRender {
 
     public RectF getKLineRect() {
         return kLineRect;
-    }
-
-    public void addMarkerView(IMarkerView markerView) {
-        highlightDrawing.addMarkerView(markerView);
     }
 
     @Override
